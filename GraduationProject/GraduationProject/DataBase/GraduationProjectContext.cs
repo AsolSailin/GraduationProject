ffbaeeb0eb45bc96f5802cb3072ca7fbeac44cee
Ruslana <garaeva-r_23@mail.ru>
2024-04-12 14:34:59 +0300

    Changing the writing of the code

--- GraduationProject/GraduationProject/DataBase/GraduationProjectContext.cs
+++ GraduationProject/GraduationProject/DataBase/GraduationProjectContext.cs
@@ -50,7 +50,6 @@ public partial class GraduationProjectContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=LAPTOP-80QITHSR\\SQLEXPRES;Initial Catalog=GraduationProject;Integrated Security=True; TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
